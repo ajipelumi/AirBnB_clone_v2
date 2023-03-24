251647866f0045f8a6ce16329f6ec73dc14390e8
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-24 18:10:58 +0100

    Test new init method

--- models/base_model.py
+++ models/base_model.py
@@ -4,9 +4,15 @@ import uuid
 from datetime import datetime
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, String, DateTime
+from os import getenv
 
 
-Base = declarative_base()
+if getenv("HBNB_TYPE_STORAGE") == 'db':
+    Base = declarative_base()
+else:
+    Base = object
+
+time_fmt = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
@@ -18,17 +24,31 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        # self.id = str(uuid.uuid4())
+        # self.created_at = datetime.now()
+        # self.updated_at = datetime.now()
+
+        # if kwargs == {}:
+        #     for key, value in kwargs.items():
+        #         if key != '__class__':
+        #             if key == "created_at" or key == "updated_at":
+        #                 value = datetime.strptime(
+        #                     value, "%Y-%m-%dT%H:%M:%S.%f")
+        #             setattr(self, key, value)
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-        if kwargs == {}:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key == "created_at" or key == "updated_at":
-                        value = datetime.strptime(
-                            value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, value)
+        self.updated_at = self.created_at
+        for key, value in kwargs.items():
+            if key == '__class__':
+                continue
+            setattr(self, key, value)
+            if type(self.created_at) is str:
+                self.created_at = datetime.strptime(
+                    self.created_at, time_fmt)
+            if type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(
+                    self.updated_at, time_fmt)
 
     def __str__(self):
         """Returns a string representation of the instance"""
