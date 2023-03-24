9656c9a21739823476f404bc015b6964fd57e793
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-24 16:59:33 +0100

    Allow default id and created values in __init__

--- models/base_model.py
+++ models/base_model.py
@@ -18,11 +18,11 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        if kwargs == {}:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key == "created_at" or key == "updated_at":
