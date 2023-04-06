63807c8bd8a3e024dd774fa98a3dd1dfce0897f0
MikeRock51 <mikerockmusic51@gmail.com>
2023-04-06 16:19:45 +0100

    Use Tee not echo ; Low Battery

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,9 +8,10 @@ sudo mkdir /data/web_static/ -p
 sudo mkdir /data/web_static/releases/ -p
 sudo mkdir /data/web_static/shared/ -p
 sudo mkdir /data/web_static/releases/test/ -p
-sudo echo "Hello Mike Rock, I'm Obidient and working" > /data/web_static/releases/test/index.html
+sudo touch /data/web_static/releases/test/index.html
+echo "Hello Mike Rock, I'm Obidient and working" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-replace="server {\n\t\tlisten 80;\n\t\tserver_name index.html;\n\t\troot /var/www/html;\n\n\t\tlocation / {\n\t\tadd_header X-Served-By hostname;\n\t\t}\n\n\t\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/hbnb_static;\n\t\t}"
+replace="server {\n\t\tlisten 80;\n\t\tserver_name index.html;\n\t\troot \/var\/www\/html;\n\n\t\tlocation \/ {\n\t\tadd_header X-Served-By hostname;\n\t\t}\n\n\t\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/hbnb_static;\n\t\t}"
 sudo sed -i "s/server {/$replace/" /etc/nginx/nginx.conf
 sudo service nginx restart
