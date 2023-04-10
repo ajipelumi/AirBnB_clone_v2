bda24389c2e4729c09ad289250c76a22a2af9d24
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-04-10 17:12:11 +0100

    j

--- h.sh
+++ h.sh
@@ -2,15 +2,15 @@
 # This script sets up our web servers for the deployment of web_static.
 
 # Update system
-sudo apt update
+apt update
 
 # Install Nginx
-sudo apt install -y nginx
+apt install -y nginx
 
 # Create necessary folders and files
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+touch /data/web_static/releases/test/index.html
 
 # Write into index.html
 content="\
@@ -23,13 +23,13 @@ content="\
 </html>"
 
 # Write content into index.html
-echo "$content" | sudo tee /data/web_static/releases/test/index.html
+echo "$content" | tee /data/web_static/releases/test/index.html
 
 # Create symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Change ownership and group to ubuntu
-sudo chown -R ubuntu:ubuntu /data/
+chown -R ubuntu:ubuntu /data/
 
 # Configure server
 SERVER_CONFIG="\
@@ -52,4 +52,4 @@ server	{
 bash -c "echo -e '$SERVER_CONFIG' > /etc/nginx/sites-enabled/default"
 
 # Restart Nginx
-sudo service nginx restart
+service nginx restart
