06b329ed0633178bb4637200b3dcc60d0545131d
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-04-25 12:17:42 +0100

    python route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" This script starts a Flask web application. """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Displays "Hello HBNB!". """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """ Displays "HBNB". """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """ Displays "C" followed by the value of the text variable. """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="is cool"):
+    """ Displays "Python" followed by the value of the text variable. """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
