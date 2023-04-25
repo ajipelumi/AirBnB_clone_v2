a9d91a18986e9f02916bfee9e1ccdc7857cab624
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-04-25 20:21:31 +0100

    state

--- models/state.py
+++ models/state.py
@@ -5,21 +5,26 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models import storage
 from models.city import City
+import os
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
+    if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
 
-    cities = relationship('City', cascade='all, delete',
+        cities = relationship('City', cascade='all, delete',
                           backref='state')
 
-    @property
-    def cities(self):
-        """ Getter for cities attribute. """
-        city_objs = []
-        for city in list(storage.all(City).values()):
-            if city.state_id == self.id:
-                city_objs.append(city)
-        return city_objs
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """ Getter for cities attribute. """
+            city_objs = []
+            for city in list(storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_objs.append(city)
+            return city_objs
